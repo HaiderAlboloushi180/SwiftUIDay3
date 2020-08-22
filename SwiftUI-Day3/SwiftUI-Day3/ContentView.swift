8cd111cb130ae624a0223c34bb798e932737a15d
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-08-23 02:17:21 +0300

    Day3 finna start

---
+++ SwiftUI-Day3/SwiftUI-Day3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Day3
+//
+//  Created by Haider Alboloushi on 8/23/20.
+//  Copyright © 2020 Haider. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
